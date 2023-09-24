8cbffa8cf70d4ed7162787b87568968776982a34
TTNhan <nhan12341184@gmail.com>
2023-09-24 12:28:36 +0700

    change redirecturl gg login

--- src/main/java/com/swp_project_g4/Service/GoogleUtils.java
+++ src/main/java/com/swp_project_g4/Service/GoogleUtils.java
@@ -28,7 +28,7 @@ public class GoogleUtils {
 
     public static String GOOGLE_CLIENT_ID = "246255507082-vpebidclj199n0sgg035cos2ijabjrmg.apps.googleusercontent.com";
     public static String GOOGLE_CLIENT_SECRET = "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";
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/swp_project_g4/loginWithGG";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/loginWithGG";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
